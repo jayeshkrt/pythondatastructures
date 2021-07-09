fa90124db227e3e02e32285d9a44a38699f12631
jayeshkrt <35258625+jayeshkrt@users.noreply.github.com>
2021-07-10 01:17:22 +0530

    Create gfg_p5.py

---
+++ gfg_p5.py
@@ -0,0 +1,6 @@
+# kth smallest/ largest element in unsorted array
+
+# for this you need to learn the quicksort, heaps, min/max heap
+# set in c++
+
+# so go back and learn that first, then come back to this problem
